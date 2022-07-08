12965fc5cac7ef1689a9a56f6436dfbf9c22430e
xieliangji <450324623@qq.com>
2022-07-09 00:59:06 +0800

    add connect-history-api-fallback middleware

--- setup.js
+++ setup.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const history = require('connect-history-api-fallback')
 
 const pm2App = express()
-pm2App.use(express.static('./dist'))
+pm2App.use(express.static('./dist')).use(history({
+    index: '/index.html'
+}))
 pm2App.listen(5000, err => {
     if (err) {
         console.log(err)
