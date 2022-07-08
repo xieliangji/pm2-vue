ae7a19d66edd894ab370dfe13e290bb3743820ae
xieliangji <450324623@qq.com>
2022-07-09 00:30:03 +0800

    add PM2 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import PM2 from '../views/PM2.vue'
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/pm2',
+    name: 'PM2',
+    component: PM2
   }
 ]
 
---
+++ src/views/PM2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="handler.add()"></button>
+  </div>
+  <div style="color: red;">{{a}}</div>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+
+const a = ref(1)
+
+const handler = {
+  add: () => {
+    a.value += 1
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
